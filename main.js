6e019619551bf32eaa5c23922ccb86b7040937ef
Yohey94 <rp0090px@ed.ritsumei.ac.jp>
2024-01-28 18:06:13 +0900

    ポートフォリオサイトの準備をしました

---
+++ main.js
@@ -0,0 +1,7 @@
+const cat1 = document.getElementById("cat1");
+const a = document.getElementById("i");
+
+cat1.onclick = function () {
+  a.src =
+    "/Users/yoheikuyama/webex/my-portfolio/6B3CB7FAE40A22A1260170784E251C4A235FC2BD.jpg1260170784E251C4A235FC2BD.jpg";
+};
